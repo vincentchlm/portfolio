ba8c6749a69c082b4c43fe7ad0c5bf9dba90d61f
Vincent Christopher Lam <114160883+vincentchlm@users.noreply.github.com>
2026-07-06 08:52:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-// Function to toggle dark mode
-function changeTheme() {
-    document.body.classList.toggle("light-mode");
-}
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav a");
+
+window.addEventListener("scroll", () => {
+
+    let current = "";
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop - 150;
+
+        if (scrollY >= sectionTop) {
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove("active");
+
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
+        }
+    });
+
+});
